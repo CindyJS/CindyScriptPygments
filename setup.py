3e1a0509b51c7cb4a16b9404ed09f5d5abb13a1e
Martin von Gagern <gagern@ma.tum.de>
2016-04-27 11:48:01 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
     name = "CindyScriptPygments",
-    version = "0.1.0",
+    version = "0.2.0",
     description = "Pygments plugin for CindyScript",
     author = "Martin von Gagern",
     author_email = "gagern@ma.tum.de",
